ee14646f165e06eb2c591645763b9ffc625e2738
iverson-lu <37316323+iverson-lu@users.noreply.github.com>
2026-01-03 18:35:12 +0800

    Redesign environment editor UI

--- src/PcTest.Ui/Views/Controls/EnvVarEditor.xaml.cs
+++ src/PcTest.Ui/Views/Controls/EnvVarEditor.xaml.cs
@@ -1,4 +1,7 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using PcTest.Ui.ViewModels;
 
 namespace PcTest.Ui.Views.Controls;
 
@@ -7,8 +10,31 @@ namespace PcTest.Ui.Views.Controls;
 /// </summary>
 public partial class EnvVarEditor : UserControl
 {
+    public static readonly DependencyProperty HintTextProperty = DependencyProperty.Register(
+        nameof(HintText),
+        typeof(string),
+        typeof(EnvVarEditor),
+        new PropertyMetadata(string.Empty));
+    
+    public string HintText
+    {
+        get => (string)GetValue(HintTextProperty);
+        set => SetValue(HintTextProperty, value);
+    }
+    
     public EnvVarEditor()
     {
         InitializeComponent();
+    }
+    
+    private void OnDeleteButtonPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (DataContext is not EnvVarEditorViewModel viewModel)
+            return;
+        
+        if (sender is Button button && button.DataContext is EnvVarRowViewModel row)
+        {
+            viewModel.SelectedItem = row;
+        }
     }
 }
